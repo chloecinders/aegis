6695177cd15bc979033e3cf262ae10d7b8fefc4c
Chloe <chloecinders@proton.me>
2026-01-10 23:53:54 +0100

    fix: disable buttons when opening modal

--- src/commands/admin/create_ocr_rule.rs
+++ src/commands/admin/create_ocr_rule.rs
@@ -117,7 +117,7 @@ impl Command for CreateOcrRule {
             }
         };
 
-        loop {
+        'outer: loop {
             let interaction = match new_msg
                 .await_component_interaction(&ctx.shard)
                 .timeout(Duration::from_secs(60 * 5))
@@ -172,99 +172,112 @@ impl Command for CreateOcrRule {
                         break;
                     }
 
-                    let interaction = match new_msg.await_modal_interaction(&ctx.shard).timeout(Duration::from_secs(600)).await {
-                        Some(s) => s,
-                        None => {
-                            break;
-                        },
-                    };
-
-                    let mut reason = String::from("No reason provided");
-                    let mut days = 1u8;
-                    let mut silent = false;
-
-                    for component in &interaction.data.components {
-                        let Some(component) = component.components.first() else { continue };
-
-                        match component {
-                            ActionRowComponent::InputText(component) if component.custom_id == "reason" => {
-                                reason = component.value.clone().unwrap_or(String::from("No reason provided"));
-                            },
-                            ActionRowComponent::InputText(component) if component.custom_id == "days" => {
-                                let str = component.value.clone().unwrap_or_default();
-                                days = str.parse::<u8>().unwrap_or(0).clamp(0, 7);
-                            },
-                            ActionRowComponent::InputText(component) if component.custom_id == "silent" => {
-                                let str = component.value.clone().unwrap_or_default();
-                                silent = matches!(
-                                    str.to_lowercase().as_str(),
-                                    "true"
-                                        | "y"
-                                        | "yes"
-                                        | "yeah"
-                                        | "t"
-                                        | "ok"
-                                        | "on"
-                                        | "enabled"
-                                        | "1"
-                                        | "enable"
-                                        | "check"
-                                        | "checked"
-                                        | "sure"
-                                        | "yep"
-                                        | "aye"
-                                        | "valid"
-                                        | "correct"
-                                );
-                            }
-                            _ => { continue }
-                        };
-                    }
-
-                    if let Err(err) = query!(r#"
-                        INSERT INTO automod_rules (id, guild_id, name, type, rule, is_regex, reason, punishment_type, day_clear_amount, silent)
-                        VALUES ($1, $2, $3, 'ocr', $4, $5, $6, 'softban', $7, $8)
-                    "#, db_id, msg.guild_id.map(|id| id.get()).unwrap_or(1) as i64, name, inner, is_regex, reason, days as i16, silent)
-                    .execute(&*SQL).await {
-                        consume_pgsql_error("OCR SOFTBAN RULE CREATE".into(), err);
-                        return Err(CommandError {
-                            title: String::from("Could not update the database"),
-                            hint: None,
-                            arg: None,
-                        });
-                    }
-
-                    {
-                        let mut lock = _handler.rule_cache.lock().await;
-                        lock.insert_ocr(Rule {
-                            name: name.clone(),
-                            id: db_id.clone(),
-                            rule: inner.to_string(),
-                            is_regex,
-                            guild_id: msg.guild_id.map(|id| id.get()).unwrap_or(1),
-                            punishment: Punishment::Softban { reason: reason.clone(), day_clear_amount: days, silent }
-                        })
-                    }
-
-                    let reply = CreateInteractionResponseMessage::new()
-                        .add_embed(
-                            CreateEmbed::new()
-                                .description(format!(
-                                    "**CREATED OCR RULE {}**\n-# ID: `{}` | Type: softban | Reason: {} | Days to clear: {} | Silent: {}\n```\n{}\n```",
-                                    name.to_uppercase(),
-                                    db_id,
-                                    clamp_chars(reason, 25),
-                                    days,
-                                    silent,
-                                    rule
-                                ))
-                                .color(BRAND_BLUE)
+                    let _ = new_msg
+                        .edit(
+                            &ctx,
+                            EditMessage::new().components(disabled_buttons.clone()),
                         )
-                        .allowed_mentions(CreateAllowedMentions::new().replied_user(false));
+                        .await;
 
-                    if let Err(err) = interaction.create_response(&ctx, CreateInteractionResponse::Message(reply)).await {
-                        consume_serenity_error("SEND CREATE OCR RULE RESPONSE".into(), err);
-                        return Ok(());
+                    loop {
+                        let interaction = match new_msg.await_modal_interaction(&ctx.shard).timeout(Duration::from_secs(600)).await {
+                            Some(s) => s,
+                            None => {
+                                break;
+                            },
+                        };
+
+                        if interaction.data.custom_id != "softban-modal" {
+                            continue 'outer;
+                        }
+
+                        let mut reason = String::from("No reason provided");
+                        let mut days = 1u8;
+                        let mut silent = false;
+
+                        for component in &interaction.data.components {
+                            let Some(component) = component.components.first() else { continue };
+
+                            match component {
+                                ActionRowComponent::InputText(component) if component.custom_id == "reason" => {
+                                    reason = component.value.clone().unwrap_or(String::from("No reason provided"));
+                                },
+                                ActionRowComponent::InputText(component) if component.custom_id == "days" => {
+                                    let str = component.value.clone().unwrap_or_default();
+                                    days = str.parse::<u8>().unwrap_or(0).clamp(0, 7);
+                                },
+                                ActionRowComponent::InputText(component) if component.custom_id == "silent" => {
+                                    let str = component.value.clone().unwrap_or_default();
+                                    silent = matches!(
+                                        str.to_lowercase().as_str(),
+                                        "true"
+                                            | "y"
+                                            | "yes"
+                                            | "yeah"
+                                            | "t"
+                                            | "ok"
+                                            | "on"
+                                            | "enabled"
+                                            | "1"
+                                            | "enable"
+                                            | "check"
+                                            | "checked"
+                                            | "sure"
+                                            | "yep"
+                                            | "aye"
+                                            | "valid"
+                                            | "correct"
+                                    );
+                                }
+                                _ => { continue }
+                            };
+                        }
+
+                        if let Err(err) = query!(r#"
+                            INSERT INTO automod_rules (id, guild_id, name, type, rule, is_regex, reason, punishment_type, day_clear_amount, silent)
+                            VALUES ($1, $2, $3, 'ocr', $4, $5, $6, 'softban', $7, $8)
+                        "#, db_id, msg.guild_id.map(|id| id.get()).unwrap_or(1) as i64, name, inner, is_regex, reason, days as i16, silent)
+                        .execute(&*SQL).await {
+                            consume_pgsql_error("OCR SOFTBAN RULE CREATE".into(), err);
+                            return Err(CommandError {
+                                title: String::from("Could not update the database"),
+                                hint: None,
+                                arg: None,
+                            });
+                        }
+
+                        {
+                            let mut lock = _handler.rule_cache.lock().await;
+                            lock.insert_ocr(Rule {
+                                name: name.clone(),
+                                id: db_id.clone(),
+                                rule: inner.to_string(),
+                                is_regex,
+                                guild_id: msg.guild_id.map(|id| id.get()).unwrap_or(1),
+                                punishment: Punishment::Softban { reason: reason.clone(), day_clear_amount: days, silent }
+                            })
+                        }
+
+                        let reply = CreateInteractionResponseMessage::new()
+                            .add_embed(
+                                CreateEmbed::new()
+                                    .description(format!(
+                                        "**CREATED OCR RULE {}**\n-# ID: `{}` | Type: softban | Reason: {} | Days to clear: {} | Silent: {}\n```\n{}\n```",
+                                        name.to_uppercase(),
+                                        db_id,
+                                        clamp_chars(reason, 25),
+                                        days,
+                                        silent,
+                                        rule
+                                    ))
+                                    .color(BRAND_BLUE)
+                            )
+                            .allowed_mentions(CreateAllowedMentions::new().replied_user(false));
+
+                        if let Err(err) = interaction.create_response(&ctx, CreateInteractionResponse::Message(reply)).await {
+                            consume_serenity_error("SEND CREATE OCR RULE RESPONSE".into(), err);
+                            return Ok(());
+                        }
                     }
 
                     break;
